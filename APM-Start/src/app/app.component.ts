4a98bf4efaf8d15bb25b59fa058c734d33a6a531
Duy Nguyen <duy.nguyen@experianinteractive.com>
2019-09-24 09:41:26 -0700

    practice

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>You got this</div>
+  </div>`
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = "Duy Duy Duy";
 }
